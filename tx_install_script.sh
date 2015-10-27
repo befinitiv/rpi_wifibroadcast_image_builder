2fb9289411cce4f92ed09d47afbd116fde9aef5d
Samuel Brucksch <Rangarid>
2015-10-27 09:53:00 +0000

    disable desktop

--- tx_install_script.sh
+++ tx_install_script.sh
@@ -26,6 +26,16 @@ sudo update-rc.d wbctxd start
 sudo bash -c "echo \"gpu_mem=128\" >> /boot/config.txt"
 sudo bash -c "echo \"start_x=1\" >> /boot/config.txt"
 
+#disable desktop
+[ -e /etc/init.d/lightdm ] && sudo update-rc.d lightdm disable 2
+if [ -e /etc/profile.d/boottoscratch.sh ]; then
+  sudo rm -f /etc/profile.d/boottoscratch.sh
+  sudo sed -i /etc/inittab \
+    -e "s/^#\(.*\)#\s*BTS_TO_ENABLE\s*/\1/" \
+    -e "/#\s*BTS_TO_DISABLE/d"
+  telinit q
+fi
+
 #change hostname
 CURRENT_HOSTNAME=`sudo cat /etc/hostname | sudo tr -d " \t\n\r"`
 NEW_HOSTNAME="wifibroadcasttx"
